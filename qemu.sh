cfd24c902438f4dc58be9f095155bf3df1abe490
Miguel Mark <mmark9@bu.edu>
2021-01-18 21:26:34 -0600

    Fix qemu launch command

--- qemu.sh
+++ qemu.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "debug" ]; then
 	echo "Debug On"
-	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios default -kernel release/sal_os_riscv32.elf -s -S
+	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios release/sal_os_riscv32_osbi_payload.elf -s -S
 else
 	echo "Debug Off"
-	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios default -kernel release/sal_os_riscv32.elf
+	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios release/sal_os_riscv32_osbi_payload.elf
 fi
