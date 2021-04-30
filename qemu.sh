e37511aa7effa8e2ee2cc6c9ef976cf7fcfa803c
Miguel Mark <mmark@tamu.edu>
2021-04-29 22:21:43 -0500

    Update qemu launcher to use jmp firmware

--- qemu.sh
+++ qemu.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "debug" ]; then
 	echo "Debug On"
-	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios release/sal_os_riscv32_osbi_payload.elf -s -S
+	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -kernel release/sal_os_riscv32_jmp.elf -bios release/sal_os_riscv32.elf -s -S
 else
 	echo "Debug Off"
-	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -bios release/sal_os_riscv32_osbi_payload.elf
+	qemu-system-riscv32 -machine virt -nographic -smp 1 -m 128M -kernel release/sal_os_riscv32_jmp.elf -bios release/sal_os_riscv32.elf
 fi
