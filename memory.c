df87ab8d9ba03bac6d334eac1eaf7fea032f76fa
Miguel Mark <mmark@tamu.edu>
2021-05-09 15:51:47 -0500

    Add additional memory setup routines
    These are mainly needed for early setup of the kernel address
    space. Eventually this may support user address space management.

--- memory.c
+++ memory.c
@@ -1,9 +1,10 @@
 #include "memory.h"
 #include "system.h"
 #include "kprint.h"
+#include "devtree.h"
 #include "constants.h"
 #include "page_table.h"
-#include "buddy_falloc.h"
+#include "page_frame.h"
 
 uint64_t memory_range_from_device_tree(const void* devtree) {
     DeviceTreeContext ctx;
@@ -38,8 +39,8 @@ void setup_memory_subsystem(uint32_t load_addr,
     uint32_t mem_end = mem_base + mem_size;
     uint32_t kernel_phys_end = load_addr + kernel_size;
     uint32_t frame_pool_start = kernel_phys_end;
+    frame_pool_start = (frame_pool_start >> 12) << 12;
     uint32_t frame_pool_size = mem_end - frame_pool_start;
-    buddy_init_from_range(frame_pool_start, frame_pool_size);
 }
 
 void map_kernel_higher_half(uint32_t load_addr,
@@ -55,7 +56,7 @@ void map_kernel_higher_half(uint32_t load_addr,
     uint32_t old_pte = page_table_lv1[pde_index];
     for (uint32_t i = 0, p = pde_index; i < num_4mb_mappings; i++,p++) {
         new_pte = sv32_kernel_pte(paddr_ptr, true,
-                                  true, true, true);
+                                  true, true, true, false);
         page_table_lv1[p] = new_pte;
         vaddr_ptr += mb4;
         paddr_ptr += mb4;
@@ -72,7 +73,7 @@ void map_device_tree_area(uint32_t device_tree_addr) {
     uint32_t tree_aligned_addr = (device_tree_addr >> 22U) << 22U;
     uint32_t tree_pte = sv32_kernel_pte(tree_aligned_addr,
                                         true, false,
-                                        false, true);
+                                        false, true, false);
     page_table_lv1[tree_pde_index] = tree_pte;
 }
 
@@ -86,25 +87,26 @@ void create_early_identity_map(uint32_t firmware_addr, uint32_t map_size) {
     for (uint32_t i = 0, p = pde_index; i < num_mappings; i++,p++) {
         pte = sv32_kernel_pte(aligned_addr,
                               true, true,
-                              true, true);
+                              true, true, false);
         page_table_lv1[p] = pte;
         aligned_addr += mb4;
     }
     // tlb flush?
 }
 
-void setup_kernel_heap(uint32_t heap_base, uint32_t heap_size) {
-    kheap_init(heap_base, heap_size);
-    init_malloc((void*)kheap_start(), kheap_size());
+void mem_do_page_table_fixup() {
+    uint32_t fixup_pte = 0;
+    uint32_t address = 0;
+    uint32_t* page_table_lv1 = page_table_lv1_ptr;
+    for (uint32_t i = 0; i < PAGE_TABLE_NR_SLOTS; i++) {
+        if (sv32_rsw(page_table_lv1[i]) == RSW_LV1_FIXUP) {
+            // convert this to a pointer
+            address = (page_table_lv1[i] >> 10) << 12;
+            fixup_pte = sv32_kernel_pte_pointer(address, true);
+            page_table_lv1[i] = fixup_pte;
+        }
+    }
 }
-
-typedef enum {
-    TEXT_SEGMENT = 0,
-    READ_ONLY_SEGMENT,
-    DATA_SEGMENT,
-    BSS_SEGMENT,
-    HEAP_SEGMENT
-} SegmentType;
 
 void mem_placement_map_segment_low(uint32_t v_addr_base,
                                    uint32_t p_addr_base,
@@ -117,7 +119,6 @@ void mem_placement_map_segment_low(uint32_t v_addr_base,
     uint32_t pde_index = 0;
     uint32_t frame_addr = 0;
     uint32_t pte_index = 0;
-    BuddyAllocResult buddy_res;
     uint32_t v_addr_ptr = v_addr_base;
     uint32_t p_addr_ptr = p_addr_base;
     uint32_t* page_table_lv1 = page_table_lv1_ptr;
@@ -127,22 +128,24 @@ void mem_placement_map_segment_low(uint32_t v_addr_base,
         pde_index = sv32_vpn1(v_addr_ptr);
         if (!sv32_is_valid(page_table_lv1[pde_index])
             || !sv32_is_leaf_page(page_table_lv1[pde_index])) {
-            buddy_res = buddy_allocate(0);
-            if (!buddy_res.valid)
-                panic("mem: no available free frames available to map segment\n");
+            frame_addr = pf_bitmap_alloc_frame();
+            if (frame_addr == 0)
+                sys_panic("mem: no available free frames available to map segment\n");
             kprintf("mem: mapping lv1 page (%p) to virtual address %p\n",
                     frame_addr, v_addr_ptr);
-            page_table_lv1[pde_index] = sv32_kernel_pte_pointer(buddy_res.address, true);
+            page_table_lv1[pde_index] = sv32_kernel_pte(frame_addr,
+                                                        true, true,
+                                                        false, true, true);
         }
         pte_index = sv32_vpn0(v_addr_ptr);
         page_table_lv2 = page_table_lv2_ptr(pde_index);
-
         if (seg_type == TEXT_SEGMENT) {
             page_table_lv2[pte_index] = sv32_kernel_pte(p_addr_ptr,
                                                         false,
                                                         false,
                                                         true,
-                                                        true);
+                                                        true,
+                                                        false);
             kprintf("mem: mapping lv2 text page (%p) to virtual address (%p)\n",
                     p_addr_ptr, v_addr_ptr);
         } else if (seg_type == DATA_SEGMENT) {
@@ -150,7 +153,8 @@ void mem_placement_map_segment_low(uint32_t v_addr_base,
                                                         true,
                                                         true,
                                                         false,
-                                                        true);
+                                                        true,
+                                                        false);
             kprintf("mem: mapping lv2 data page (%p) to virtual address (%p)\n",
                     p_addr_ptr, v_addr_ptr);
         } else if (seg_type == READ_ONLY_SEGMENT) {
@@ -158,7 +162,8 @@ void mem_placement_map_segment_low(uint32_t v_addr_base,
                                                         true,
                                                         false,
                                                         false,
-                                                        true);
+                                                        true,
+                                                        false);
             kprintf("mem: mapping lv2 read-only page (%p) to virtual address (%p)\n",
                     p_addr_ptr, v_addr_ptr);
         } else if (seg_type == BSS_SEGMENT) {
@@ -166,15 +171,117 @@ void mem_placement_map_segment_low(uint32_t v_addr_base,
                                                         true,
                                                         true,
                                                         false,
-                                                        true);
+                                                        true,
+                                                        false);
             kprintf("mem: mapping lv2 bss page (%p) to virtual address (%p)\n",
                     p_addr_ptr, v_addr_ptr);
         } else {
-            panic("mem: mapping heap segment not supported yet");
+            sys_panic("mem: mapping heap segment not supported yet");
         }
         v_addr_ptr += PAGE_SIZE;
         p_addr_ptr += PAGE_SIZE;
     }
+    mem_do_page_table_fixup();
+}
+
+void mem_map_segment_low(uint32_t v_addr_base,
+                                   uint32_t nr_pages,
+                                   SegmentType seg_type) {
+    // maps a segment which is a contiguous region
+    // containing the same protection attributes
+    // segments must start at a page boundary
+    uint32_t pde_index = 0;
+    uint32_t frame_addr = 0;
+    uint32_t pte_index = 0;
+    uint32_t v_addr_ptr = v_addr_base;
+    uint32_t* page_table_lv1 = page_table_lv1_ptr;
+    uint32_t* page_table_lv2 = page_table_lv2_ptr(0);
+    for (uint32_t i = 0; i < nr_pages; i++) {
+        // check if we need to set a PDE
+        pde_index = sv32_vpn1(v_addr_ptr);
+        if (!sv32_is_valid(page_table_lv1[pde_index])
+            || !sv32_is_leaf_page(page_table_lv1[pde_index])) {
+            frame_addr = pf_bitmap_alloc_frame();
+            if (frame_addr == 0)
+                sys_panic("mem: no available free frames available to map segment\n");
+            kprintf("mem: mapping lv1 page (%p) to virtual address %p\n",
+                    frame_addr, v_addr_ptr);
+            // we temporarily map this as a leaf page for reverse mapping
+            page_table_lv1[pde_index] = sv32_kernel_pte(frame_addr, true,
+                                                        true, false, true,true);
+        }
+        pte_index = sv32_vpn0(v_addr_ptr);
+        page_table_lv2 = page_table_lv2_ptr(pde_index);
+        frame_addr = pf_bitmap_alloc_frame();
+        if (frame_addr == 0)
+            sys_panic("mem: failed to allocated more pages for segment map");
+        if (seg_type == TEXT_SEGMENT) {
+            page_table_lv2[pte_index] = sv32_kernel_pte(frame_addr,
+                                                        false,
+                                                        false,
+                                                        true,
+                                                        true,
+                                                        false);
+            kprintf("mem: mapping lv2 text page (%p) to virtual address (%p)\n",
+                    frame_addr, v_addr_ptr);
+        } else if (seg_type == DATA_SEGMENT) {
+            page_table_lv2[pte_index] = sv32_kernel_pte(frame_addr,
+                                                        true,
+                                                        true,
+                                                        false,
+                                                        true,
+                                                        false);
+            kprintf("mem: mapping lv2 data page (%p) to virtual address (%p)\n",
+                    frame_addr, v_addr_ptr);
+        } else if (seg_type == READ_ONLY_SEGMENT) {
+            page_table_lv2[pte_index] = sv32_kernel_pte(frame_addr,
+                                                        true,
+                                                        false,
+                                                        false,
+                                                        true,
+                                                        false);
+            kprintf("mem: mapping lv2 read-only page (%p) to virtual address (%p)\n",
+                    frame_addr, v_addr_ptr);
+        } else if (seg_type == BSS_SEGMENT) {
+            page_table_lv2[pte_index] = sv32_kernel_pte(frame_addr,
+                                                        true,
+                                                        true,
+                                                        false,
+                                                        true,
+                                                        false);
+            kprintf("mem: mapping lv2 bss page (%p) to virtual address (%p)\n",
+                    frame_addr, v_addr_ptr);
+        } else {
+            sys_panic("mem: mapping heap segment not supported yet");
+        }
+        v_addr_ptr += PAGE_SIZE;
+    }
+    mem_do_page_table_fixup();
+}
+
+void setup_kernel_heap(uint32_t heap_base, uint32_t heap_size) {
+    kheap_init(heap_base, heap_size);
+    uint32_t pages_needed = heap_size / FRAME_SIZE;
+    uint32_t size_remain = heap_size % FRAME_SIZE;
+    if (size_remain > 0)
+        pages_needed += 1;
+    mem_map_segment_low(heap_base, pages_needed, DATA_SEGMENT);
+    init_malloc((void*)kheap_start(), kheap_size());
+}
+
+void mem_map_leaf_page(uint32_t phys_addr, uint32_t vaddr, SegmentType seg_type) {
+    mem_placement_map_segment_low(vaddr, phys_addr, 1, seg_type);
+}
+void mem_unmap_leaf_page(uint32_t vaddr) {
+    uint32_t* page_table_lv1 = page_table_lv1_ptr;
+    uint32_t pde_index = sv32_vpn1(vaddr);
+    if (!sv32_is_valid(page_table_lv1[pde_index])
+        || !sv32_is_leaf_page(page_table_lv1[pde_index])) {
+        sys_panic("mem: level 1 page not mapped to page table");
+    }
+    uint32_t pte_index = sv32_vpn0(vaddr);
+    uint32_t* page_table_lv2 = page_table_lv2_ptr(pde_index);
+    page_table_lv2[pte_index] = 0;
 }
 
 void mem_placement_map_segment(uint32_t p_start,
@@ -198,23 +305,24 @@ void mem_setup_page_table(KernelLinkerConfig* config) {
     // assume no second level pages exist
     // assume that we have a recursive PTE in the last of the page table
     // MAP TEXT SEGMENT
-    uint32_t text_v_addr = config->kernel_virtual_base;
+    uint32_t text_v_addr = config->kernel_text_begin_virtual;
     uint32_t kernel_text_size = config->kernel_text_end - config->kernel_text_begin;
     mem_placement_map_segment(config->kernel_text_begin, text_v_addr, kernel_text_size, TEXT_SEGMENT);
     // MAP RO SEGMENT
     // TODO: verify that size is a multiple of frame size
-    uint32_t ro_v_addr = text_v_addr + kernel_text_size;
+    uint32_t ro_v_addr = config->kernel_ro_data_begin_virtual;
     uint32_t kernel_ro_size = config->kernel_ro_data_end - config->kernel_ro_data_begin;
     mem_placement_map_segment(config->kernel_ro_data_begin, ro_v_addr, kernel_ro_size, READ_ONLY_SEGMENT);
     // MAP DATA SEGMENT
     // TODO: verify that size is a multiple of frame size
-    uint32_t data_v_addr = ro_v_addr + kernel_ro_size;
+    uint32_t data_v_addr = config->kernel_data_begin_virtual;
     uint32_t kernel_data_size = config->kernel_ro_data_end - config->kernel_ro_data_begin;
     mem_placement_map_segment(config->kernel_data_begin, data_v_addr, kernel_data_size, DATA_SEGMENT);
     // MAP BSS SEGMENT
     // TODO: verify that size is a multiple of frame size
-    uint32_t bss_v_addr = data_v_addr + kernel_data_size;
-    uint32_t kernel_bss_size = config->kernel_bss_end - config->kernel_bss_begin;
-    mem_placement_map_segment(config->kernel_data_begin, data_v_addr, kernel_data_size, DATA_SEGMENT);
+    uint32_t bss_v_addr = config->kernel_bss_begin_virtual;
+    // FIXME: why is the mapping off by one?
+    uint32_t kernel_bss_size = config->kernel_bss_end - config->kernel_bss_begin + FRAME_SIZE;
+    mem_placement_map_segment(config->kernel_data_begin, bss_v_addr, kernel_bss_size, DATA_SEGMENT);
     // TODO: map heap?
 }
--- memory.h
+++ memory.h
@@ -14,10 +14,26 @@ typedef struct {
     uint32_t kernel_data_end;
     uint32_t kernel_bss_begin;
     uint32_t kernel_bss_end;
+    uint32_t kernel_text_begin_virtual;
+    uint32_t kernel_text_end_virtual;
+    uint32_t kernel_ro_data_begin_virtual;
+    uint32_t kernel_ro_data_end_virtual;
+    uint32_t kernel_data_begin_virtual;
+    uint32_t kernel_data_end_virtual;
+    uint32_t kernel_bss_begin_virtual;
+    uint32_t kernel_bss_end_virtual;
     uint32_t kernel_virtual_base;
     uint32_t kernel_virtual_end;
     uint32_t kernel_size_in_bytes;
 } KernelLinkerConfig;
+
+typedef enum {
+    TEXT_SEGMENT = 0,
+    READ_ONLY_SEGMENT,
+    DATA_SEGMENT,
+    BSS_SEGMENT,
+    HEAP_SEGMENT
+} SegmentType;
 
 uint64_t memory_range_from_device_tree(const void* devtree);
 void setup_memory_subsystem(uint32_t load_addr,
@@ -31,4 +47,6 @@ void map_device_tree_area(uint32_t device_tree_addr);
 void create_early_identity_map(uint32_t firmware_addr, uint32_t map_size);
 void setup_kernel_heap(uint32_t heap_base, uint32_t heap_size);
 void mem_setup_page_table(KernelLinkerConfig* config);
+void mem_map_leaf_page(uint32_t phys_addr, uint32_t vaddr, SegmentType seg_type);
+void mem_unmap_leaf_page(uint32_t vaddr);
 #endif //RISCV_MICROK_MEMORY_H
