d9b1933d6812823ce63363df3669f777ce11794e
Miguel Mark <mmark@tamu.edu>
2021-05-09 16:27:40 -0500

    Update kernel entry point

--- kernel_main.c
+++ kernel_main.c
@@ -1,16 +1,10 @@
-#include "csr.h"
 #include "sched.h"
-#include "kmalloc.h"
-#include "page_table.h"
-#include "kheap.h"
-#include "bit_utils.h"
-#include "buddy_falloc.h"
 #include "kprint.h"
+#include "system.h"
 #include "interrupt.h"
 #include "sbi_relay.h"
-#include "buddy_falloc.h"
-#include "page_frame.h"
 #include "memory.h"
+#include "page_frame.h"
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -27,6 +21,27 @@ void print_banner() {
     kputs(BANNER);
 }
 
+void thread_func1() {
+    for (uint32_t i = 0; i < 100; i++) {
+        kputs("running in thread 1...\n");
+        // hacky yield
+        if (i % 20 == 0)
+            sys_ecall();
+    }
+}
+
+void thread_func2() {
+    for (uint32_t i = 0; i < 200; i++) {
+        kputs("running in thread 2...\n");
+        // hacky yield
+        if (i % 10 == 0)
+            sys_ecall();
+    }
+}
+
+struct thread_tcb thread1;
+struct thread_tcb thread2;
+
 void kernel_main(
         uint32_t hart_id,
 		void* config,
@@ -36,39 +51,49 @@ void kernel_main(
     struct sbiret impl_id = sbi_relay_get_impl_id();
     struct sbiret march_id = sbi_relay_get_marchid();
     struct sbiret vendor_id = sbi_relay_get_mvendorid();
-    kprintf("boot: running on core %d | march_id = %d "
+    kprintf("system: running on core %d | march_id = %d "
             "| vendor_id = %d | impl_id = %d\n", hart_id,
             march_id.value, vendor_id.value, impl_id.value);
+    kprintf("boot: kernel image size %d\n", linker_config.kernel_size_in_bytes);
     // for now map 16 MiB
     uint32_t identity_map_size = (0x1 << 22U) * 8;
     // map 8 MiB for kernel area initially
+    kputs("system: setting up supervisor level interrupts..\n");
+    setup_trap_handlers();
+    sys_enable_supervisor_interrupts();
+    sys_enable_supervisor_timer_interrupts();
+    sys_enable_supervisor_software_interrupts();
     uint32_t higher_half_map_size = identity_map_size / 2;
-    kputs("boot: setting up early kernel address space.\n");
+    kputs("system: setting up early kernel address space.\n");
     create_early_identity_map(linker_config.firmware_load_address,
                               identity_map_size);
     map_device_tree_area((uint32_t)config);
-    kputs("boot: loading memory config from device tree..\n");
+    kputs("system: loading memory config from device tree..\n");
     uint64_t mem_range = memory_range_from_device_tree(config);
     uint32_t mem_base = mem_range >> 32;
     uint32_t mem_size = UINT32_MAX & mem_range;
-    kprintf("boot: memory_start = %p | memory_size = %u\n",
+    kprintf("system: memory_start = %p | memory_size = %u\n",
             mem_base, mem_size);
-    kputs("boot: initializing memory subsystem...\n");
-    setup_memory_subsystem(linker_config.kernel_load_address,
-                           mem_base, mem_size,
-                           higher_half_map_size);
+    kputs("system: setting up frame bitmap...\n");
+    init_frame_bitmap_from_linker_config(&linker_config);
     kprintf("boot: setting up higher half of address space...\n");
     mem_setup_page_table(&linker_config);
     // TODO: make heap setup flexible
     uint32_t kheap_size = (0x1U << 20U); // 1MiB
     uint32_t kheap_start = linker_config.kernel_virtual_base +
             higher_half_map_size - kheap_size;
-    kprintf("boot: setting heap @ %p | size = %u bytes\n",
+    // aligned address
+    kheap_start = (kheap_start >> 12) << 12;
+    kprintf("system: setting heap @ %p | size = %u bytes\n",
             kheap_start, kheap_size);
     setup_kernel_heap(kheap_start, kheap_size);
-    kputs("boot: setting up supervisor level interrupts..\n");
-    setup_trap_handlers();
-    enable_interrupts();
+    kputs("system: initializing scheduler\n");
+    sched_init();
+    kputs("system: starting first thread\n");
+    sched_init_thread(&thread1, thread_func1);
+    sched_init_thread(&thread2, thread_func2);
+    sched_enqueue(&thread1);
+    sched_enqueue(&thread2);
     // at this point we either jump to another thread
     // or just wait for the timer interrupt to fire
     while (true) {
