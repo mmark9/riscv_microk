74491ceaebed3d6008c4401649f9983cf32c854d
Miguel Mark <mmark@tamu.edu>
2021-05-09 15:46:48 -0500

    Improve system level interface
    Abstractions to control the system should be concentrated into
    a signle translation unit to simplify maintenance.

--- system.c
+++ system.c
@@ -1,20 +1,91 @@
+#include "csr.h"
 #include "kprint.h"
 #include "sbi_relay.h"
+#include "system.h"
 
 #include <stdbool.h>
 
-void panic(const char* reason) {
-    kprintf("!!kernel panic!!: %s\n", reason);
+void sys_ecall() {
+    __asm__("ecall");
+}
+void sys_kassert(bool condition) {
+    if (!condition)
+        sys_panic("assertion failed");
+}
+void sys_panic(const char* reason) {
+    kprintf("!!kernel sys_panic!!: %s\n", reason);
     sbi_relay_system_reset(SBI_SHUTDOWN,
                            SBI_RESET_SYSTEM_FAILURE);
 }
-
-void shutdown() {
-    kprintf("system: shutdown started\n");
-    sbi_relay_system_reset(SBI_SHUTDOWN, SBI_RESET_NO_REASON);
+void sys_shutdown() {
+    kprintf("system: sys_shutdown started\n");
+    sbi_relay_system_reset(SBI_SHUTDOWN,
+                           SBI_RESET_NO_REASON);
 }
-
-void kassert(bool condition) {
-    if (!condition)
-        panic("assertion failed");
+void sys_set_timer(uint64_t expire_time) {
+    uint64_t time = time_r_csr();
+    uint64_t new_time = time + expire_time;
+    struct sbiret timer_res = sbi_relay_set_timer(new_time);
+    sys_kassert(timer_res.error == SBI_SUCCESS);
+}
+void sys_stop_timer() {
+    uint64_t new_time = -1ULL;
+    struct sbiret timer_res = sbi_relay_set_timer(new_time);
+    sys_kassert(timer_res.error == SBI_SUCCESS);
+}
+void sys_enable_supervisor_interrupts() {
+    uint32_t sstatus = sstatus_r_csr();
+    uint32_t new_sstatus = sstatus_set_sie(sstatus, 1U);
+    sstatus_w_csr(new_sstatus);
+}
+void sys_disable_supervisor_interrupts() {
+    uint32_t sstatus = sstatus_r_csr();
+    uint32_t new_sstatus = sstatus_set_sie(sstatus, 0U);
+    sstatus_w_csr(new_sstatus);
+}
+void sys_enable_supervisor_timer_interrupts() {
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_stie(sie, 1U);
+    sie_w_csr(new_sie);
+}
+void sys_disable_supervisor_timer_interrupts() {
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_stie(sie, 0U);
+    sie_w_csr(new_sie);
+}
+void sys_enable_supervisor_external_interrupts() {
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_seie(sie, 1U);
+    sie_w_csr(new_sie);
+}
+void sys_disable_supervisor_external_interrupts() {
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_seie(sie, 0U);
+    sie_w_csr(new_sie);
+}
+void sys_enable_supervisor_software_interrupts(){
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_ssie(sie, 1U);
+    sie_w_csr(new_sie);
+}
+void sys_disable_supervisor_software_interrupts() {
+    uint32_t sie = sie_r_csr();
+    uint32_t new_sie = sie_set_ssie(sie, 0U);
+    sie_w_csr(new_sie);
+}
+bool sys_supervisor_interrupts_enabled() {
+    uint32_t sstatus = sstatus_r_csr();
+    return sstatus_sie(sstatus) == 1;
+}
+bool sys_supervisor_timer_interrupts_enabled() {
+    uint32_t sie = sie_r_csr();
+    return sie_stie(sie) == 1;
+}
+bool sys_supervisor_software_interrupts_enabled() {
+    uint32_t sie = sie_r_csr();
+    return sie_ssie(sie) == 1;
+}
+bool sys_supervisor_external_interrupts_enabled() {
+    uint32_t sie = sie_r_csr();
+    return sie_seie(sie) == 1;
 }
--- system.h
+++ system.h
@@ -1,10 +1,26 @@
 #ifndef RISCV_MICROK_SYSTEM_H
 #define RISCV_MICROK_SYSTEM_H
 
+#include <stdint.h>
 #include <stdbool.h>
 
-void shutdown();
-void kassert(bool condition);
-void panic(const char* reason);
+void sys_shutdown();
+void sys_kassert(bool condition);
+void sys_panic(const char* reason);
+void sys_ecall();
+void sys_stop_timer();
+void sys_enable_supervisor_interrupts();
+void sys_disable_supervisor_interrupts();
+void sys_enable_supervisor_timer_interrupts();
+void sys_disable_supervisor_timer_interrupts();
+void sys_enable_supervisor_external_interrupts();
+void sys_disable_supervisor_external_interrupts();
+void sys_enable_supervisor_software_interrupts();
+void sys_disable_supervisor_software_interrupts();
+void sys_set_timer(uint64_t expire_time);
+bool sys_supervisor_interrupts_enabled();
+bool sys_supervisor_timer_interrupts_enabled();
+bool sys_supervisor_software_interrupts_enabled();
+bool sys_supervisor_external_interrupts_enabled();
 
 #endif //RISCV_MICROK_SYSTEM_H
