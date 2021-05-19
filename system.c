3e8f5d9b8c0c46f080ae289aef68b89423f71810
Miguel Mark <mmark@tamu.edu>
2021-05-19 16:13:26 -0500

    Halt with infinite loop when panicking
    Halting with infinite loops is more ideal in hindsight because
    we may still want to investigate the cause of the trap.

--- system.c
+++ system.c
@@ -5,6 +5,9 @@
 
 #include <stdbool.h>
 
+PrivilegeMode sys_privilege_mode;
+PrivilegeMode sys_previous_privilege_mode;
+
 void sys_ecall() {
     __asm__("ecall");
 }
@@ -15,10 +18,12 @@ void sys_kassert(bool condition) {
     if (!condition)
         sys_panic("assertion failed");
 }
+void sys_halt() {
+    for (;;);
+}
 void sys_panic(const char* reason) {
     kprintf("!!kernel sys_panic!!: %s\n", reason);
-    sbi_relay_system_reset(SBI_SHUTDOWN,
-                           SBI_RESET_SYSTEM_FAILURE);
+    sys_halt();
 }
 void sys_shutdown() {
     kprintf("system: sys_shutdown started\n");
@@ -64,6 +69,26 @@ void sys_disable_supervisor_software_interrupts() {
     uint32_t sie = sie_r_csr();
     uint32_t new_sie = sie_set_ssie(sie, 0U);
     sie_w_csr(new_sie);
+}
+void sys_initialize_privilege_mode_status() {
+    sys_previous_privilege_mode = MACHINE_MODE;
+    sys_privilege_mode = SUPERVISOR_MODE;
+}
+void sys_prepare_switch_to_user() {
+    uint32_t ssatus = sstatus_r_csr();
+    uint32_t mpp = sstatus_spp(ssatus);
+    uint32_t new_sstatus = sstatus_set_spp(ssatus, 0);
+    sstatus_w_csr(new_sstatus);
+    // Note this won't be accurate until a return interrupt
+    sys_previous_privilege_mode = sys_privilege_mode;
+    sys_privilege_mode = USER_MODE;
+}
+PrivilegeMode sys_target_privilege_mode() {
+    // target privilege mode may or may not correspond
+    // to the effective privilege mode of the processor
+    // RISC-V doesn't provide a means of checking so
+    // this a compromise
+    return sys_privilege_mode;
 }
 bool sys_supervisor_interrupts_enabled() {
     uint32_t sstatus = sstatus_r_csr();
--- system.h
+++ system.h
@@ -4,11 +4,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum {
+    MACHINE_MODE = 0,
+    SUPERVISOR_MODE,
+    USER_MODE
+} PrivilegeMode;
+
 void sys_shutdown();
 void sys_kassert(bool condition);
 void sys_panic(const char* reason);
 void sys_ecall();
 void sys_ebreak();
+void sys_halt();
 void sys_stop_timer();
 void sys_enable_supervisor_interrupts();
 void sys_disable_supervisor_interrupts();
@@ -18,6 +25,9 @@ void sys_enable_supervisor_external_interrupts();
 void sys_disable_supervisor_external_interrupts();
 void sys_enable_supervisor_software_interrupts();
 void sys_disable_supervisor_software_interrupts();
+void sys_initialize_privilege_mode_status();
+void sys_prepare_switch_to_user();
+PrivilegeMode sys_target_privilege_mode();
 bool sys_supervisor_interrupts_enabled();
 bool sys_supervisor_timer_interrupts_enabled();
 bool sys_supervisor_software_interrupts_enabled();
